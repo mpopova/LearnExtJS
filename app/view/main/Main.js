e67d0b5b4496df48a71c26b8d5adb5048edb6576
Marina Popova <mpopova@Marinas-Mac-mini.local>
2017-11-07 15:51:55 +0200

    Beautifying js

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -4,132 +4,132 @@ Ext.define('QuickStart.view.main.Main', {
     //    requires: [
     //     'Ext.data.proxy.Direct'
     // ],
-    items: [
-    {
+    items: [{
         title: 'Components',
         iconCls: 'x-fa fa-th',
-         layout: {
-            type: 'vbox',
-            align: 'stretch'
-        },
+        // layout: {
+        //     type: 'vbox',
+        //     align: 'stretch'
+        // },
         items: [{
             xtype: 'container',
             flex: 1,
-            layout: { 
+            layout: {
                 type: 'hbox',
                 align: 'stretch'
             },
             items: [{
                 xtype: 'panel',
                 title: '1',
-                html:'First component',
-                flex: 1},
-            {
+                html: 'First component',
+                flex: 1
+            }, {
                 xtype: 'panel',
                 title: '2',
-                html:'Second component',
-                flex: 1}]},
-        {
+                html: 'Second component',
+                flex: 1
+            }]
+        }, {
             xtype: 'container',
             flex: 1,
-            layout: { 
+            layout: {
                 type: 'hbox',
                 align: 'stretch'
             },
             items: [{
                 xtype: 'panel',
                 title: '3',
-                html:'Third component',
-                flex: 1},]},]
+                html: 'Third component',
+                flex: 1
+            }, ]
+        }, ]
 
-    },
-    {
+    }, {
         title: 'Current weather in Sofia',
         extend: 'Ext.Component',
         singleton: true,
         xtype: 'component',
         id: 'ariTest',
-        html:'test',
-        
-//         listeners: {
-//             staterestore: function() {
-//                 console.log(this);
-// //                console.log(state)
-//             }
-//         },
+        html: 'test',
 
-        handler: function(){
-                    var treeStore = Ext.getStore('Weather');
-                    var bla;
-                    console.log(treeStore);
-                    // treeStore.sync();
-                },
-        
-//        store: {
-//            type: 'weather',
-//            autoLoad: true
-//        },
-//        
-//            
-//         initComponent: function() {
-//                this.on('afterrender', this.onAfterRender);
-//            },
-//
-//            onAfterRender: function() {
-//                 var store = Ext.data.StoreManager.lookup("weatherStore"); 
-//       
-//                    console.log(store);
-//            },
-//        listeners: {
-//            initComponent: function() {
-//                 var store = Ext.data.StoreManager.lookup("weatherStore"); 
-//       
-//                    console.log(store);
-//            }
-//        },
+        //         listeners: {
+        //             staterestore: function() {
+        //                 console.log(this);
+        // //                console.log(state)
+        //             }
+        //         },
 
-//        tpl: new Ext.Template([
-//    '<div name="sss">',
-//        '<span class="sdd">sss</span>',
-//    '</div>',
-//        
-//        ]),
-//        compile: true,
+        handler: function() {
+            var treeStore = Ext.getStore('Weather');
+            var bla;
+            console.log(treeStore);
+            // treeStore.sync();
+        },
+
+        //        store: {
+        //            type: 'weather',
+        //            autoLoad: true
+        //        },
+        //        
+        //            
+        //         initComponent: function() {
+        //                this.on('afterrender', this.onAfterRender);
+        //            },
+        //
+        //            onAfterRender: function() {
+        //                 var store = Ext.data.StoreManager.lookup("weatherStore"); 
+        //       
+        //                    console.log(store);
+        //            },
+        //        listeners: {
+        //            initComponent: function() {
+        //                 var store = Ext.data.StoreManager.lookup("weatherStore"); 
+        //       
+        //                    console.log(store);
+        //            }
+        //        },
+
+        //        tpl: new Ext.Template([
+        //    '<div name="sss">',
+        //        '<span class="sdd">sss</span>',
+        //    '</div>',
+        //        
+        //        ]),
+        //        compile: true,
         iconCls: 'x-fa fa-sun-o',
 
-//        columns: [{
-//            text: 'Location',
-//            dataIndex: 'name',
-//            flex: 1
-//        },
-//        {
-//            text: 'imageAndTxt',
-//            dataIndex: 'imageAndTxt',
-//             cell: {
-//                encodeHtml: false
-//            },
-//             renderer: function (value) {
-//                    var str = value;
-//                    var res = str.split(",");
-//                    var url = res[0];
-//                    var txt = res[1];   
-//                    var result = '<img src="' + url + '" />' + '<span> ' + txt + '</span>';
-//                    return result;
-//                },
-//            flex: 1
-//        },
-//        {
-//            text: 'Temperature',
-//            dataIndex: 'temp',
-//            flex: 1
-//        }
-//        ]
-    },
-            {
+        //        columns: [{
+        //            text: 'Location',
+        //            dataIndex: 'name',
+        //            flex: 1
+        //        },
+        //        {
+        //            text: 'imageAndTxt',
+        //            dataIndex: 'imageAndTxt',
+        //             cell: {
+        //                encodeHtml: false
+        //            },
+        //             renderer: function (value) {
+        //                    var str = value;
+        //                    var res = str.split(",");
+        //                    var url = res[0];
+        //                    var txt = res[1];   
+        //                    var result = '<img src="' + url + '" />' + '<span> ' + txt + '</span>';
+        //                    return result;
+        //                },
+        //            flex: 1
+        //        },
+        //        {
+        //            text: 'Temperature',
+        //            dataIndex: 'temp',
+        //            flex: 1
+        //        }
+        //        ]
+    }, {
         title: 'Forecast for Sofia',
         xtype: 'grid',
         iconCls: 'x-fa fa-chevron-right',
-         store: {
+        store: {
             type: 'forecast',
             autoLoad: true
         },
@@ -137,19 +137,14 @@ Ext.define('QuickStart.view.main.Main', {
             text: 'Date',
             dataIndex: 'forecastDate',
             flex: 1
-        },
-        {
+        }, {
             text: 'Min temp',
             dataIndex: 'minT',
             flex: 1
-        },
-        {
+        }, {
             text: 'Max temp',
             dataIndex: 'maxT',
             flex: 1
-        }
-        ]
-    }
-    ]  
+        }]
+    }]
 });
-
